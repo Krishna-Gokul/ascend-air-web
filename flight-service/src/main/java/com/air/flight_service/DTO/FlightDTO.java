8d49ce2cbfd5473f17bdbe1866658e3afec0bfb6
Krishna-Gokul <krishnagokul.go@gmail.com>
2025-05-04 00:09:16 +0530

    updated flight DTO with lombok

--- flight-service/src/main/java/com/air/flight_service/DTO/FlightDTO.java
+++ flight-service/src/main/java/com/air/flight_service/DTO/FlightDTO.java
@@ -1,5 +1,26 @@
 package com.air.flight_service.DTO;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class FlightDTO {
 
+	private String flightNo;
+	
+	private String flightName;
+	
+	private String flightMake;
+	
+	private Integer flightCapacity;
+	
+	private Integer availableSeats;
+	
+	private String flightOrigin;
+	
+	private LocalDateTime depatureTime;
+	
+	private LocalDateTime arrivalTime;
+	
 }
